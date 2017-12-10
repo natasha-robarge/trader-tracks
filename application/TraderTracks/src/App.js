ca652c95f370f5fedade70e82e58ddc55e197370
Natasha Robarge <robarge.natasha7@gmail.com>
2017-12-10 08:20:02 -0600

    Build failed, I think due to first react native attempt

--- application/TraderTracks/src/App.js
+++ application/TraderTracks/src/App.js
@@ -1,8 +1,19 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import firebase from 'firebase';
 
 class App extends Component {
   render() {
+    const config = {
+      apiKey: "AIzaSyA0YF5fxf8YXM1LCjw2PPiZhwxWwFU-03o",
+      authDomain: "tradertracks-5f6af.firebaseapp.com",
+      databaseURL: "https://tradertracks-5f6af.firebaseio.com",
+      projectId: "tradertracks-5f6af",
+      storageBucket: "tradertracks-5f6af.appspot.com",
+      messagingSenderId: "234575797753"
+    };
+    firebase.initializeApp(config);
+    
     return (
       <View>
         <Text>Hello World!</Text>
